60f25a225cc7301de3892627fae06abd8102d5ce
Quang Hung <hungpham307@gmail.com>
2023-06-22 21:33:22 +0700

    test adding new author

--- eBookStore/Controllers/AuthorController.cs
+++ eBookStore/Controllers/AuthorController.cs
@@ -1,39 +1,59 @@
 ﻿using eBookStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 
 namespace eBookStore.Controllers
 {
 	public class AuthorController : Controller
 	{
-		protected string _connectionString = "https://localhost:7058/api/Authors";
-		// GET: AuthorController
-		public async Task<ActionResult> Index()
+		private readonly HttpClient _client;
+		private string _connectionString = "";
+
+        public AuthorController()
+        {
+            _client = new HttpClient();
+        }
+		
+        // GET: AuthorController
+        public async Task<IActionResult> Index()
+        {
+            _connectionString = "https://localhost:7058/api/Authors";
+
+            HttpResponseMessage response = await _client.GetAsync(_connectionString);
+            string strData = await response.Content.ReadAsStringAsync();
+            List<Author> listAuthors = JsonSerializer.Deserialize<List<Author>>(strData);
+
+            return View(listAuthors);
+        }
+
+        public async Task<ActionResult> CreateAuthor(string firstName, string lastName, string emailAddress, string city)
 		{
-			List<Author> authors = new List<Author>();
+            _connectionString = "https://localhost:7058/api/Authors";
+			
+            var request = new Author
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress,
+                City = city
+            };
+			
+            string json = JsonSerializer.Serialize(request);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _client.PostAsync(_connectionString, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewData["Error"] = "Error adding author";
+            }
 
-			using (HttpClient client = new HttpClient())
-			{
-				using (HttpResponseMessage res = await client.GetAsync(_connectionString))
-				{
-					using (HttpContent content = res.Content)
-					{
-						string data = await content.ReadAsStringAsync();
-						// deserialize data
-						authors = JsonSerializer.Deserialize<List<Author>>(data);
-					}
-				}
-			}
-			ViewBag.ListAuthor = authors;
-
-			return View();
-		}
-
-		// GET: AuthorController/Details/5
-		public ActionResult Details(int id)
-		{
-			return View();
+            return RedirectToAction("Index");
 		}
 
 		// GET: AuthorController/Create
