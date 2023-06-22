ec0d66a39523b212f8ce4ae4bbfd3b0e9cbde086
Quang Hung <hungpham307@gmail.com>
2023-06-22 17:24:10 +0700

    configure book mapping

---
+++ BusinessObject/Common/Mappings/BookMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BusinessObject.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Common.Mappings
+{
+    public class BookMapping : Profile
+    {
+        public BookMapping()
+        {
+            CreateMap<Book, BookDto>()
+                .ForMember(dest => dest.PublisherName,
+               opt => opt.MapFrom(src => src.Publisher.PublisherName));
+        }
+    }
+}
